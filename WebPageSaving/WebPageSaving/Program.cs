500226b62225e41a3b51e7af787423847f4ced90
Qupipab <54556157+Qupipab@users.noreply.github.com>
2020-03-09 19:19:13 +0200

    Saver Update

--- WebPageSaving/WebPageSaving/Program.cs
+++ WebPageSaving/WebPageSaving/Program.cs
@@ -12,40 +12,56 @@ namespace WebPageSaving
 
             string[] commandsArr = Console.ReadLine().Split(new char[] { ' ' });
             string command = commandsArr[0];
+            string siteName = commandsArr[1].Split('/')[0];
             string sitePath = commandsArr[1];
+
+            if (commandsArr[1].IndexOf("/") != -1)
+            {
+                sitePath = commandsArr[1].Substring(commandsArr[1].IndexOf('/'));
+            }
+
             string filePath = "..\\..\\..\\SavedFiles\\" + commandsArr[2];
 
             if (command == "pagesaver")
             {
-                string msg = "GET / HTTP/1.0\nHost: " + sitePath + "\n\n";
+                string msg = $"GET /{sitePath} HTTP/1.0\r\nHost: {siteName}\r\n\r\n";
                 try
                 {
                     var port = 80;
-                    var serverAddr = sitePath;
-                    var client = new TcpClient(serverAddr, port);
+                    var client = new TcpClient(siteName, port);
                     var data = System.Text.Encoding.ASCII.GetBytes(msg);
                     NetworkStream stream = client.GetStream();
 
                     stream.Write(data, 0, data.Length);
                     stream.Flush();
 
-                    var responseData = new byte[1024];
+                    var responseData = new byte[4096];
                     int bytesRead = stream.Read(responseData, 0, responseData.Length);
                     var responseMessage = System.Text.Encoding.ASCII.GetString(responseData, 0, bytesRead);
                     stream.Close();
                     client.Close();
 
-                    responseMessage = responseMessage.Substring(responseMessage.IndexOf("<!Doctype"));
+                    if (responseMessage.IndexOf("<html>", StringComparison.OrdinalIgnoreCase) > -1)
+                    {
+                        int searchIndex = responseMessage.IndexOf("<html>", StringComparison.OrdinalIgnoreCase);
+                        responseMessage = responseMessage.Substring(searchIndex);
+                    }
+                    else if (responseMessage.IndexOf("<header>", StringComparison.OrdinalIgnoreCase) > -1)
+                    {
+                        int searchIndex = responseMessage.IndexOf("<header>", StringComparison.OrdinalIgnoreCase);
+                        responseMessage = responseMessage.Substring(searchIndex);
+                    }
+
                     using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.Default))
                     {
                         sw.WriteLine(responseMessage);
                     }
+                    Console.WriteLine("Запись выполнена");
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Excep {0}", e);
                 }
-                Console.WriteLine("Запись выполнена");
             }
         }
     }
